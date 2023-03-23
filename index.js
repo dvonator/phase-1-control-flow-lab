1c3108d3dc6865f5cc453bf0e7079bd4f70568e3
David <davidvo1992@gmail.com>
2023-03-23 17:06:24 -0500

    to submit controls lab

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(somenumber) {
   // Write your code here!
+  let result;
+  if (somenumber <= 400) {
+    result = "This one is on me!";
+  } else if (somenumber > 400 && somenumber < 2000) {
+    result = "That will be twenty bucks.";
+  } else if (somenumber > 2500) {
+    result = "No can do.";
+  } else {
+    result = "I will gladly take your thirty bucks.";
+  }
+
+  return result;
+}
+somenumber(199);
+
+function ternaryCheckCity(somecity) {
+  // Write your code here!
+  if (somecity === "NYC") {
+    return "Ok, sounds good.";
+  } else {
+    return "No go.";
+  }
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(customertip) {
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (customertip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    case "thanks for everything":
+      return "Bye.";
+      break;
+  }
 }
